57b91f296115d280d6a45278d3fbcc5509606def
plzgirlfriend <yongjin2258@gmail.com>
2024-09-05 09:45:11 +0900

    project init

---
+++ src/main/java/main/gospring/GospringApplication.java
@@ -0,0 +1,13 @@
+package main.gospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/gospring/GospringApplicationTests.java
@@ -0,0 +1,13 @@
+package main.gospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
